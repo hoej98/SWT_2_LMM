fdb6db2df8a4b5d872600211ca16bcb711838dcd
Mikkel Høj Hansen <mikkelhoejhansen@gmail.com>
2020-10-21 12:31:24 +0200

    Tilføjede alle tests for klassen Display

--- LadeskabTest/UnitTest1.cs
+++ LadeskabTest/UnitTest1.cs
@@ -8,6 +8,8 @@ namespace LadeskabTest
 
     public class TestLadeskab
     {
+
+#region DisplayTest
         [Test]
         public void ShowConnectPhone_IsCorrect()
         {
@@ -38,10 +40,69 @@ namespace LadeskabTest
             Assert.That(uut.msg == "Tryk 'R' for at indtaste RFID");
         }
 
+        [Test]
+        public void ShowRFIDError_IsCorrect()
+        {
+            // arrange
+            var uut = new Display();
 
+            //act
+            uut.showRFIDError();
 
+            //uut.Add(7, 17);
 
+            //Assert
+            Assert.That(uut.msg == "Forkert RFID tag");
+        }
 
+        [Test]
+        public void ShowRemovePhone_IsCorrect()
+        {
+            // arrange
+            var uut = new Display();
+
+            //act
+            uut.showRemovePhone();
+
+            //uut.Add(7, 17);
+
+            //Assert
+            Assert.That(uut.msg == "Tag din telefon ud af skabet og luk døren");
+        }
+
+        [Test]
+        public void ShowConnectionError_IsCorrect()
+        {
+            // arrange
+            var uut = new Display();
+
+            //act
+            uut.showConnectionError();
+
+            //uut.Add(7, 17);
+
+            //Assert
+            Assert.That(uut.msg == "Din telefon er ikke ordentlig tilsluttet. Prøv igen.");
+        }
+
+        [Test]
+        public void ShowConfirmation_IsCorrect()
+        {
+            // arrange
+            var uut = new Display();
+
+            //act
+            uut.showConfirmation();
+
+            //uut.Add(7, 17);
+
+            //Assert
+            Assert.That(uut.msg == "Skabet er låst og din telefon lades. Brug dit RFID tag til at låse op.");
+        }
+
+        #endregion
+
+#region DoorTest
         [Test]
         public void OnDoorOpen_IsCorrect()
         {
@@ -54,6 +115,7 @@ namespace LadeskabTest
             //Assert
             //Assert.That(uut.msg == "Tilslut din telefon");
         }
+        #endregion
 
     }
 }
