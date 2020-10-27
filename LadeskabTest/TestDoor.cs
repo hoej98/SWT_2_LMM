1a95ae6346a905b759fa7ef3f6855f08ff434a17
lhpj <PremiumLasse@live.dk>
2020-10-27 22:15:18 +0100

    Allersidste fiks mener det den her gang

--- LadeskabTest/TestDoor.cs
+++ LadeskabTest/TestDoor.cs
@@ -33,26 +33,6 @@ namespace LadeskabTest
         }
 
         [Test]
-        public void OnDoorOpen_EventNotFired()
-        {
-            // Arrange
-            var uut = new Door();
-            DoorEventArgs receivedEventArgs = null;
-
-            uut.DoorChangedEvent +=
-                (o, args) =>
-                {
-                    receivedEventArgs = args;
-                };
-
-            // Act
-            // Do nothing -- event should NOT be fired
-
-            // Assert
-            Assert.That(receivedEventArgs, Is.Null);
-        }
-
-        [Test]
         public void OnDoorClose_EventFired()
         {
             // Arrange
@@ -70,26 +50,6 @@ namespace LadeskabTest
 
             // Assert
             Assert.That(receivedEventArgs, Is.Not.Null);
-        }
-
-        [Test]
-        public void OnDoorClose_EventNotFired()
-        {
-            // Arrange
-            var uut = new Door();
-            DoorEventArgs receivedEventArgs = null;
-
-            uut.DoorChangedEvent +=
-                (o, args) =>
-                {
-                    receivedEventArgs = args;
-                };
-
-            // Act
-            // Do nothing -- event should NOT be fired
-
-            // Assert
-            Assert.That(receivedEventArgs, Is.Null);
         }
 
         [Test]
